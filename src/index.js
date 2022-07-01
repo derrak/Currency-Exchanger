c6987efb91221ea1fcdbe153ab2aff7fbe8628b2
Derrak Richard <derrak@gmail.com>
2022-07-01 14:16:35 -0700

    update index.js with document ready functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
-/* eslint-disable */
 import $ from 'jquery';
-import Template from './js/template.js';
-/* eslint-enable */
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchangeRate from './services/currency-service.js';
+
+
+function clearFields() {
+  $('#value1').val("");
+  $('.show-errors').text("");
+}
+
+function displayConversionResult(conversionResult) {
+  $('.result').text(`The result is ${conversionResult}!`);
+}
+
+function displayErrors(error) {
+  $('.show-errors').text(`${error}`);
+}
+
+$(document).ready(function() {
+  $('#btn-convert').click(function() {
+    let baseCurrency = "USD";    
+    let targetCurrency = $('#convertToCountry').val();
+    let valueToConvert = $('#value1').val();
+    clearFields();
+    CurrencyExchangeRate.getConversionValue(baseCurrency, targetCurrency, valueToConvert)
+      .then(function(currencyResponse) {
+        if (currencyResponse instanceof Error) {
+          throw Error(`Exchange Rate API error: ${currencyResponse.message}`);
+        }
+        const conversionResult = currencyResponse.conversion_result;
+        displayConversionResult(conversionResult);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
+  });
+});
