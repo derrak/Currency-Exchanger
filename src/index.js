45371bda46a839afd26cb5f5d2121134272a38c6
Derrak Richard <derrak@gmail.com>
2022-07-02 21:08:07 -0700

    update clearFields function and docReady functions

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import CurrencyExchangeRate from './services/currency-service.js';
 
 function clearFields() {
   $('#value1').val("");
+  $('.result').text("");
   $('.show-errors').text("");
 }
 
@@ -26,56 +27,10 @@ async function makeApiCall(baseCurrency,targetCurrency,valueToConvert) {
 $(document).ready(function() {
   $('#btn-convert').click(function() {
     event.preventDefault();
-    clearFields()
-    let baseCurrency = "USD";    
-    // let targetCurrency = $('#convertToCountry').val();
-    let targetCurrency = "CAD";
-    // let valueToConvert = $('#value1').val();
-    let valueToConvert = "88888888";
-    // clearFields();
+    let baseCurrency = "usd";
+    let targetCurrency = $('#convertToCountry').val();
+    let valueToConvert = $('#value1').val();
+    clearFields();
     makeApiCall(baseCurrency,targetCurrency,valueToConvert);
   });
 });
-
-
-
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-// import CurrencyExchangeRate from './services/currency-service.js';
-
-
-// function clearFields() {
-//   $('#value1').val("");
-//   $('.show-errors').text("");
-// }
-
-// function displayConversionResult(conversionResult) {
-//   $('.result').text(`The result is ${conversionResult}!`);
-// }
-
-// function displayErrors(error) {
-//   $('.show-errors').text(`${error}`);
-// }
-
-// $(document).ready(function() {
-//   $('#btn-convert').click(function() {
-//     let baseCurrency = "USD";    
-//     let targetCurrency = $('#convertToCountry').val();
-//     let valueToConvert = $('#value1').val();
-//     //clearFields();
-//     CurrencyExchangeRate.getConversionValue(baseCurrency, targetCurrency, valueToConvert)
-//       .then(function(currencyResponse) {
-//         console.log(currencyResponse);
-//         if (currencyResponse instanceof Error) {
-//           throw Error(`Exchange Rate API error: ${currencyResponse.message}`);
-//         }
-//         const conversionResult = currencyResponse.conversion_result;
-//         displayConversionResult(conversionResult);
-//       })
-//       .catch(function(error) {
-//         displayErrors(error.message);
-//       });
-//   });
-// });
